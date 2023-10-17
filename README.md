86533f74be3a55c359fd0d21bcb7aaf119c52708
cifal shaul <cifalshaul373@gmail.com>
2023-10-17 22:32:00 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+A simple bookstore application using MERN Stack
---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://cifalshaul373:cifalthegreat@bookstore-mern.tve0sd8.mongodb.net/books-collection?retryWrites=true&w=majority'
